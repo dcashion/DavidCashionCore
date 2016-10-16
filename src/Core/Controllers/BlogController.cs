69ead1af367d352258e21f3770f10fb1f6aceae4
David Cashion <dcashion@outlook.com>
2016-10-16 17:04:50 -0600

    Updated BlogController

--- src/Core/Controllers/BlogController.cs
+++ src/Core/Controllers/BlogController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Core.Controllers
 {
-    public class HomeController : Controller
+    public class BlogController : Controller
     {
         public IActionResult Index()
         {
